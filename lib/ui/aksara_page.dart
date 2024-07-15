b655fabfb49f72111558a8e367f5056ff3b4112b
rikin <rikinugraha@student.telkomuniversity.ac.id>
2024-07-15 19:06:22 +0700

    final

--- lib/ui/aksara_page.dart
+++ lib/ui/aksara_page.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class AksaraPage extends StatelessWidget {
   AksaraPage({super.key});
@@ -9,6 +11,34 @@ class AksaraPage extends StatelessWidget {
       List.generate(10, (index) => 'assets/angka/1bb${index}.PNG');
   final List<String> vokal =
       List.generate(7, (index) => 'assets/swara/1b${83 + index}.PNG');
+  List<String> _vokal = ['a', 'i', 'u', 'é', 'o', 'e', 'eu'];
+  List<String> _ngalagena = [
+    'ka',
+    'qa',
+    'ga',
+    'nga',
+    'ca',
+    'ja',
+    'ya',
+    'ra',
+    'la',
+    'wa',
+    'sa',
+    'xa',
+    'za',
+    'nya',
+    'ta',
+    'da',
+    'na',
+    'pa',
+    'fa',
+    'va',
+    'ba',
+    'ma',
+    'ha',
+    'kha',
+    'sya'
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -24,43 +54,88 @@ class AksaraPage extends StatelessWidget {
     return SingleChildScrollView(
       child: Column(
         children: [
+          Padding(
+            padding: EdgeInsets.all(8),
+            child: Text(
+              "Vokal/Swara",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ),
+          GridView.builder(
+            physics: NeverScrollableScrollPhysics(),
+            shrinkWrap: true,
+            gridDelegate:
+                SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+            itemCount: vokal.length,
+            itemBuilder: (context, index) {
+              return Card(
+                child: Column(
+                  children: [
+                    Expanded(
+                      child: Center(
+                          child: Image.asset(
+                        vokal[index],
+                        fit: BoxFit.cover,
+                      )),
+                    ),
+                    Center(
+                      child: Text(
+                        _vokal[index],
+                        style: TextStyle(fontSize: 24),
+                      ),
+                    )
+                  ],
+                ),
+              );
+            },
+          ),
+          SizedBox(
+            height: 30,
+          ),
+           Padding(
+            padding: EdgeInsets.all(8),
+            child: Text(
+              "Ngalagena",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ),
           GridView.builder(
             physics: NeverScrollableScrollPhysics(),
             shrinkWrap: true,
             gridDelegate:
                 SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 4),
-            itemCount: vokal.length,
-            itemBuilder: (context, index) {
-              return Card(
-                child: Center(
-                  child: Image.asset(
-                  vokal[index],
-                  fit: BoxFit.cover,
-                )),
-              );
-            },
-          ),
-          SizedBox(
-            height: 30,
-          ),
-          GridView.builder(
-            physics: NeverScrollableScrollPhysics(),
-            shrinkWrap: true,
-            gridDelegate:
-                SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 5),
             itemCount: ngalagena.length,
             itemBuilder: (context, index) {
               return Card(
-                child: Center(
-                    child: Image.asset(
-                  ngalagena[index],
-                  fit: BoxFit.cover,
-                )),
+                child: Column(
+                  children: [
+                    Expanded(
+                      child: Center(
+                          child: Image.asset(
+                        ngalagena[index],
+                        fit: BoxFit.cover,
+                      )),
+                    ),
+                    Center(
+                      child: Text(
+                        _ngalagena[index],
+                        style: TextStyle(fontSize: 18),
+                      ),
+                    )
+                  ],
+                ),
               );
             },
           ),
           SizedBox(
             height: 30,
+          ),
+           Padding(
+            padding: EdgeInsets.all(8),
+            child: Text(
+              "Angka",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
           ),
           GridView.builder(
             physics: NeverScrollableScrollPhysics(),
@@ -70,11 +145,21 @@ class AksaraPage extends StatelessWidget {
             itemCount: angka.length,
             itemBuilder: (context, index) {
               return Card(
-                child: Center(
-                    child: Image.asset(
-                  angka[index],
-                  fit: BoxFit.cover,
-                )),
+                child: Column(
+                  children: [
+                    Expanded(
+                      child: Center(
+                          child: Image.asset(
+                        angka[index],
+                        fit: BoxFit.cover,
+                      )),
+                    ),
+                    Center(
+                      child: Text(index.toString(),
+                          style: TextStyle(fontSize: 18)),
+                    )
+                  ],
+                ),
               );
             },
           ),
